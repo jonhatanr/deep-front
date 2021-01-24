caab97f9a159b3ba46b2108a9619d47602330658
jonhat7n <jonhatanricardor@gmail.com>
2021-01-24 13:47:02 -0600

    Change landing page to 2 theme style

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -4,7 +4,7 @@
         <div class="position-relative">
             <!-- shape Hero -->
             <section class="section-shaped my-0">
-                <div class="shape shape-style-3 shape-default shape-skew bg-gradient-warning">
+                <div class="shape shape-style-2 shape-default shape-skew bg-gradient-warning">
                     <span></span>
                     <span></span>
                     <span></span>
